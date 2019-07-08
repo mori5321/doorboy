da0d4098e3766c6a5dbbd1390e6f666fddf5056b
mori5321 <moriwm77@gmail.com>
2019-07-08 14:33:09 +0900

    a

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Doorman.MixProject do
   use Mix.Project
 
+  @description """
+  Simple and Light-Weight Library for Authorization made with Elixir
+  """
+
   def project do
     [
       app: :doorman,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Doorman",
+      description: @description,
+      package: package,
     ]
   end
 
@@ -23,6 +30,14 @@ defmodule Doorman.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["mori"],
+      licenses: ["MIT"],
+      links: %{ "Github" => "https://github.com/mori5321/doorman" }
     ]
   end
 end
