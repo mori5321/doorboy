ef8697ffa2e6bbf91c6d43d5c730abceb24c9c6e
mori5321 <moriwm77@gmail.com>
2019-10-03 17:49:53 +0900

    add doc

--- lib/doorboy.ex
+++ lib/doorboy.ex
@@ -1,7 +1,38 @@
 
 defmodule Doorboy do
+  @moduledoc """
+  Doorboy main module made with macro.
+  """
+
+  @doc """
+  Use this module in your Phoenix Controller.
+
+  ## Example
+    defmodule SampleController do
+      use Doorboy, policy: SamplePolicy
+
+      def create(conn, params)
+        book = Book.fetch(params.book_id)
+        {:ok, _} <- authorize :create, user: conn.current_user, book: book
+      end
+    end
+  
+  
+  Policy file looks like this.
+  
+  ## Example
+    defmodule SamplePolicy do
+      
+      // If this function returns true, authorize/2 returns {:ok, nil}
+      // If this function returns false, authorize/2 returns {:error, :unauthorized}
+      def create(user: user, book: book) do
+        user && user.id == book.user_id
+      end
+    end
+  
+  """
   defmacro __using__(policy: policy) do
-    quote do 
+    quote do
       def authorize(function, opts) do
         case _authorize(function, opts) do
           true -> {:ok, nil}
